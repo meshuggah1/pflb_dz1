a4b61b813d8e54e7ce8ce76d9742fb36f076361e
meshuggah <Kostia-Ku61@yandex.ru>
2019-12-19 07:53:56 +0300

    created project structure

---
+++ src/main/java/ru/kudryavcev_kb/dz_1/Main.java
@@ -0,0 +1,7 @@
+package ru.kudryavcev_kb.dz_1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/ru/kudryavcev_kb/dz_1/TestMain.java
@@ -0,0 +1,4 @@
+package ru.kudryavcev_kb.dz_1;
+
+public class TestMain {
+}
